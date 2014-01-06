c4f2ee611f22d6c6c6f2d4de18d9f41696defefd
Katelyn Sills <katelynsills@gmail.com>
2014-01-05 20:17:10 -0500

    First

---
+++ tweetadvice.py
@@ -0,0 +1,36 @@
+import twitter
+import random
+import os
+import re
+
+#open the source file
+
+guide = open('gentsguide.txt', 'r').read()
+
+#divide the source file up into tweetable sentences
+guide = re.split('\.|\?', guide)
+print guide
+
+#pick a random quote
+max = len(guide)
+length = 200
+while length > 140 or length < 1:
+	quote = guide[random.randint(0,max-1)].strip()
+	length = len(quote)
+
+print quote
+print len(quote)
+
+"""
+api = twitter.Api(consumer_key=os.environ['T_CONSUMER_KEY'],
+				  consumer_secret=os.environ['T_CONSUMER_SECRET'],
+				  access_token_key=os.environ['T_ACCESS_TOKEN_KEY'],
+				  access_token_secret=os.environ['T_ACCESS_TOKEN_SECRET'])
+api.PostUpdates(quote)
+"""
+
+
+#attempt to shorten it?
+#post a random quote (4 times a day?)
+#delete the random quote from the source file?
+
